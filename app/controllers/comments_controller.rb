e9913872b5552fddbb64667a9fd8abdd467f1297
Mirek <mirek@szul.eu>
2018-10-04 01:00:41 +0200

    Delete unnecessary code from comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
-  before_action :find_movie
-  before_action :find_comment, only: [:edit, :destroy]
+  before_action :authenticate_user!
+  before_action :find_movie, only: [:create, :destroy]
 
   # def index
   #   @comments = Comment.all
@@ -15,33 +15,37 @@ class CommentsController < ApplicationController
     @comment.user_id = current_user.id
 
     if @comment.save
-      redirect_to movie_path(@movie), notice: "Your comment was successfully created!"
+      flash[:notice] = "Your comment was successfully created!"
+      redirect_to movie_path(@movie)
     else
-      redirect_to movie_path(@movie), notice: "Your comment has not been created!"
+      flash[:danger] = "Your comment has not been created!"
+      redirect_to movie_path(@movie)
     end
   end
 
-  def update
-    if @comment.update(comment_params)
-      redirect_to @comment, notice: "Your comment was successfully updated!"
-    else
-      render 'edit'
-    end
-  end
+  # def update
+  #   if @comment.update(comment_params)
+  #     redirect_to @comment, notice: "Your comment was successfully updated!"
+  #   else
+  #     render 'edit'
+  #   end
+  # end
 
   def destroy
-    @comment.destroy
-    redirect_to movies_path, notice: "Your comment was successfully deleted!"
+    @comment = @movie.comments.find(params[:id])
+    if @comment.destroy
+      flash[:success] = "Your comment was successfully deleted!"
+      redirect_to movie_path(@movie)
+    else
+      flash[:danger] = "Your comment has not been deleted!"
+      redirect_to movie_path(@movie)
+    end
   end
 
   private
 
   def comment_params
     params.require(:comment).permit(:description, :movie_id, :user_id)
-  end
-
-  def find_comment
-    @comment = Comment.find(params[:id])
   end
 
   def find_movie
