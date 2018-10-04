80e8a7ded11d431410b1edc5ca493a67288bbcf1
Mirek <mirek@szul.eu>
2018-10-04 23:59:33 +0200

    Add comment model spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,8 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Comment do
+  it { should belong_to(:user) }
+  it { should belong_to(:movie) }
+
+  it { should validate_presence_of(:description) }
 end
